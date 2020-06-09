bab3faf8fd2ed71c1764b6a83ae61cd85b8d4cd5
Jonathan Fleckenstein <jonfleck88@gmail.com>
2020-06-09 13:12:01 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import webpack from "webpack";
+import { DOMParser } from "xmldom";
+import fs from "fs-extra";
+import eol from "eol";
+
+export default class TypedSVGSymbols {
+  public apply(compiler: webpack.Compiler) {
+    compiler.hooks.emit.tap(
+      "CompileSvgSprite",
+      (compilation: webpack.compilation.Compilation) => {
+        const sprites = [
+          ...compilation.fileDependencies,
+        ].filter((fileName: string) => fileName.endsWith("sprite.svg"));
+
+        sprites.forEach((filePath) => {
+          const domParser = new DOMParser();
+          const doc = domParser.parseFromString(
+            fs.readFileSync(filePath).toString(),
+            "image/svg+xml"
+          );
+
+          const ids = Array.from(
+            doc.getElementsByTagName("symbol")
+          ).map((symbol) => symbol.getAttribute("id"));
+
+          fs.writeFileSync(
+            `${filePath}.d.ts`,
+            eol.lf(`
+declare const _default: string;
+export default _default;
+          
+export const symbol: ${ids.map((id) => `"${id}"`).join(" | ")};
+          `)
+          );
+        });
+      }
+    );
+  }
+}
